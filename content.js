9adec91646591d7891832b018c07776f63cb3816
vandercoder <andikawilliam@outlook.com>
2020-08-01 14:58:09 +0700

    initial commit version 3

---
+++ content.js
@@ -0,0 +1,64 @@
+let rubyOnRails = [
+  [
+    "/resources/rails-twitter/Homepage.png",
+    "Twitterr",
+    "Twitterr on Rails",
+    "Made a website that imitates the core functionalities of Twitter. Users can login and post tweets that will appear in the timeline.",
+    "https://github.com/vandercoder/micro-twitter",
+    "https://shrouded-temple-93524.herokuapp.com"
+  ],
+  [
+    "/resources/rails-eventmaster/Home.png",
+    "Event Master",
+    "Event Master",
+    "Website mock-up based on Event Brite. Users can login and post events online. They can also invite other users and join available events.",
+    "https://github.com/vandercoder/event-master",
+    "https://cryptic-stream-58688.herokuapp.com",
+  ],
+  [
+    "/resources/rails-flight_booker/Home.png",
+    "Flight Booker",
+    "Flight Booker",
+    "Flight booking prototype website. User is able to search available flights and book them. Still ongoing project..",
+    "",
+    ""
+  ]
+];
+
+let django = [
+  [
+    "/resources/django-kelasku/Home.png",
+    "Kelasku",
+    "Kelasku",
+    "Course Review website. Students can login and give review to courses they have been to. Students can see reviews of other courses to decide which ones are better to take.",
+    "https://github.com/vandercoder/kelasku",
+    "https://fast-beach-26927.herokuapp.com",
+  ]
+];
+
+let javascript = [
+  [
+    "/resources/javascript-calculator/Home.png",
+    "Calculator",
+    "Calculator",
+    "Exercise project on performing basic math operations using javascript. Also an exercise for implementing event-listeners",
+    "https://github.com/vandercoder/calculator",
+    "https://vandercoder.github.io/calculator/"
+  ],
+  [
+    "/resources/javascript-cashier_program/Home.png",
+    "Cashier Program",
+    "Cashier Program",
+    "A step-up from the calculator project. Cashier counting webpage, add in available products and count their sum. ",
+    "https://github.com/vandercoder/cashier-program",
+    "https://vandercoder.github.io/cashier-program/"
+  ],
+  [
+    "/resources/javascript-sketch_pad/sketch-pad.png",
+    "Sketch Pad",
+    "Sketch Pad",
+    "Another exercise on event listeners. A simple sketching pad using your mouse as the pen. Can also modify the number of grid boxes for sketching.",
+    "https://github.com/vandercoder/sketch-pad",
+    "https://vandercoder.github.io/sketch-pad/"
+  ]
+];
---
+++ index.js
@@ -0,0 +1,22 @@
+$(".navburger").click(function() {
+  $(".navmenu").toggleClass("relative");
+  $(".navmenu").toggleClass("hidden");
+});
+
+
+
+// create cover letter
+// create template for summary
+
+// to do:
+// populate homepage
+// add footer Find me
+// add about
+// add gallery
+// smooth drop down navbar mobile
+
+// separate data (advanced using yaml)
+
+// done:
+// separate data (advanced using yaml)
+// add changing smooth animation (fade in out)
