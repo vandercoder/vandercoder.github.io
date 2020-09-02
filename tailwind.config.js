956fbcab9ceec23c04d6da20caa541860ae6a5a6
vandercoder <andikawilliam@outlook.com>
2020-09-02 17:39:00 +0700

    added changes

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto' : ['Roboto', 'sans-serif'],
+        'lato' : ['Lato', 'sans-serif'],
+      },
+      fontSize: {
+        '1vw' : '1vw',
+        '1.2vw' : '1.2vw',
+        '1.5vw' : '1.5vw',
+        '2vw' : '2vw',
+      },
+      height: {
+        '1/3': '33vh',
+        '2/5': '40vh',
+        '3/5': '60vh',
+        '1/2': '50vh',
+        'video-full': '77vh',
+      },
+      minHeight: {
+        'halfscreen': '50vh',
+      },
+      opacity: {
+        '40': '0.4',
+        '60': '0.6'
+      },
+    },
+    
+  },
+  variants: {},
+  plugins: [],
+}
