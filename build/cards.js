2b81dd222c878cd5e171a422178717bc15b7828c
vandercoder <andikawilliam@outlook.com>
2020-09-12 22:33:48 +0700

    fix position

--- build/cards.js
+++ build/cards.js
@@ -5,14 +5,18 @@ let template = document.querySelector("#card-template");
 
 techStacks.forEach(insertProjects);
 
-$(".tech-stack").click(function() {
+$(".tech-stack").click(function () {
   if (!this.classList.contains("active")) {
-    $(".tech-stack.active").addClass("border-black text-gray-800 hover:bg-gray-400 hover:text-gray-800");
+    $(".tech-stack.active").addClass(
+      "border-black text-gray-800 hover:bg-gray-400 hover:text-gray-800"
+    );
     $(".tech-stack.active").removeClass("active bg-gray-800 text-white");
-    $(this).removeClass("border-black hover:border-gray-200 text-gray-800 hover:bg-gray-200");
+    $(this).removeClass(
+      "border-black hover:border-gray-200 text-gray-800 hover:bg-gray-200"
+    );
     $(this).addClass("active bg-gray-800 text-white");
 
-    let stack = getStack(this)
+    let stack = getStack(this);
     changeStack(stack);
   }
 });
@@ -20,11 +24,9 @@ $(".tech-stack").click(function() {
 function getStack(button) {
   if (button.classList.contains("ror")) {
     index = 0;
-  } 
-  else if (button.classList.contains("django")) {
+  } else if (button.classList.contains("django")) {
     index = 1;
-  } 
-  else {
+  } else {
     index = 2;
   }
   return index;
@@ -35,55 +37,56 @@ function changeStack(stack) {
   let oldCards = $(projectCards).children();
 
   // projectCards.querySelectorAll('*').forEach(n => n.remove());
-  $(oldCards).fadeOut(600, function() {
+  $(oldCards).fadeOut(600, function () {
     $(this).remove();
     insertProjects(stackProjects);
   });
   return false;
-};
+}
 
 function insertProjects(stackProjects) {
   let div = document.createElement("div");
   // $(div).addClass("container mx-auto md:flex md:justify-center")
   // $(div).hide()
 
-  stackProjects.forEach(function(projectContent) {
+  stackProjects.forEach(function (projectContent) {
     let card = template.content.cloneNode(true);
     let cardMain = card.querySelector(".card-main");
-  
+
     // let img = card.querySelector("img");
     // img.setAttribute("src", projectContent[0]);
     // img.setAttribute("alt", projectContent[1]);
 
-    cardMain.style.backgroundImage = "url(/resources/videos/screenshot/" + projectContent[0] + ".png)"
+    cardMain.style.backgroundImage =
+      "url(/resources/videos/screenshot/" + projectContent[0] + ".png)";
 
     let videoSrc = card.querySelector("video source");
-    let videoPath = "./resources/videos/" + projectContent[0] + ".mp4"
+    let videoPath = "./resources/videos/" + projectContent[0] + ".mp4";
     videoSrc.setAttribute("src", videoPath);
-    
+
     let cardTitle = card.querySelector(".card-title");
     cardTitle.textContent = projectContent[2];
-    
+
     let cardContent = card.querySelector(".card-content");
     cardContent.textContent = projectContent[3];
 
     let cardStack = card.querySelector(".card-stack");
-    cardStack.textContent = "Stack: " + projectContent[4]
-    
+    cardStack.textContent = "Stack: " + projectContent[4];
+
     let liveLink = card.querySelector(".live-link");
-    liveLink.setAttribute("href", projectContent[5])
+    liveLink.setAttribute("href", projectContent[5]);
 
     let cardText = card.querySelector(".card-text");
     let overlay = card.querySelector(".overlay");
     let video = card.querySelector("video");
 
-    cardMain.addEventListener("mouseover", function(){
+    cardMain.addEventListener("mouseover", function () {
       cardText.style.transform = "scale(1.3)";
       overlay.classList.remove("opacity-60");
       overlay.classList.add("opacity-40");
       video.play();
     });
-    cardMain.addEventListener("mouseout", function(){
+    cardMain.addEventListener("mouseout", function () {
       cardText.style.transform = "scale(1.0)";
       overlay.classList.remove("opacity-40");
       overlay.classList.add("opacity-60");
@@ -92,7 +95,7 @@ function insertProjects(stackProjects) {
 
     let loadMessage = card.querySelector(".load-message");
 
-    cardMain.addEventListener("click", function(){
+    cardMain.addEventListener("click", function () {
       loadMessage.textContent = projectContent[6];
     });
 
--- build/content.js
+++ build/content.js
@@ -6,8 +6,8 @@ let django = [
     "Course Review website. Students can login and give review to courses they have been to.",
     "Django, Boostrap",
     "https://fast-beach-26927.herokuapp.com",
-    "Opening... Please wait a moment, the server takes time to load.."
-  ]
+    "Opening... Please wait a moment, the server takes time to load..",
+  ],
 ];
 
 let rubyOnRails = [
@@ -18,7 +18,7 @@ let rubyOnRails = [
     "Made a website that imitates the core functionalities of Twitter.",
     "Ruby on Rails, Devise, Bulma",
     "https://shrouded-temple-93524.herokuapp.com",
-    "Opening... Please wait a moment, the server takes time to load.."
+    "Opening... Please wait a moment, the server takes time to load..",
   ],
   [
     "ror-event-master",
@@ -27,7 +27,7 @@ let rubyOnRails = [
     "Website mock-up based on Event Brite. Users can login and post events online.",
     "Ruby on Rails, Devise, Bulma",
     "https://cryptic-stream-58688.herokuapp.com",
-    "Opening... Please wait a moment, the server takes time to load.."
+    "Opening... Please wait a moment, the server takes time to load..",
   ],
 ];
 
@@ -39,7 +39,7 @@ let javascript = [
     "Exercise project on performing basic math operations using javascript. ",
     "plain JS",
     "https://vandercoder.github.io/calculator/",
-    ""
+    "",
   ],
   [
     "js-cashier-program",
@@ -48,7 +48,7 @@ let javascript = [
     "Cashier counting webpage, add in available products and count their sum. ",
     "plain JS",
     "https://vandercoder.github.io/cashier-program/",
-    ""
+    "",
   ],
   [
     "js-sketch-pad",
@@ -57,6 +57,6 @@ let javascript = [
     "A simple sketching pad using your mouse as the pen.",
     "plain JS",
     "https://vandercoder.github.io/sketch-pad/",
-    ""
-  ]
+    "",
+  ],
 ];
